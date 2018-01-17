3a439c19f5dffe8b8776fb103e7fe535666393ff
Simon Brady <simonbrady@mac.com>
2018-01-16 21:28:16 -0500

    Add imgs for styling

--- src/components/candidates.js
+++ src/components/candidates.js
@@ -22,23 +22,25 @@ class Candidates extends React.Component {
             </ReactRevealText>
           </h1>
         <div className="pageText">
-          <p>
-            Did you know that the average speed your resume is screened by the corporate recruiter is 7 seconds or less?
-          </p>
-          <div className="clock">
+          <div className="clockText">
+            <ReactRevealText show={this.state.show}>
+              Did you know that the average speed your resume is screened by the corporate recruiter is 7 seconds or less?
+            </ReactRevealText>
+          </div>
             <ReactCountDownClock seconds ={7}
               color = 'orange'
               alpha={0.9}
               fontSize='auto'
-              size={250}
+              size={400}
             />
+          <div className="candidateText">
+            <p>
+              Having been in the corporate world for multiple years, our staff consultants know how to help you market your resume to the role of your choice.
+            </p>
+            <p> 
+              Secondly, an ally for you should be someone who is transparent about your skills, knows the industry you are in or has colleagues that have personally experienced it. Chances are no one recruiter can be all you have in obtaining the role or new career. We are in the business of aligning relations.
+            </p>
           </div>
-          <p>
-            Having been in the corporate world for multiple years, our staff consultants know how to help you market your resume to the role of your choice.
-          </p>
-          <p> 
-            Secondly, an ally for you should be someone who is transparent about your skills, knows the industry you are in or has colleagues that have personally experienced it. Chances are no one recruiter can be all you have in obtaining the role or new career. We are in the business of aligning relations.
-          </p>
         </div>
       </div>
     );
